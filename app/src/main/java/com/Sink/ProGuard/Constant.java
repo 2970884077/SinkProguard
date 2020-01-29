f5631ab51fe197adf735a629627a178f1ae12a26
DisableSnow <468766131@qq.com>
2020-01-29 10:46:56 +0800

    提交插件文件

--- app/src/main/java/com/Sink/ProGuard/Constant.java
+++ app/src/main/java/com/Sink/ProGuard/Constant.java
@@ -8,6 +8,7 @@ public class Constant
 	public static final String PLUGIN_PATH = MAIN_PATH+"plugin/";
 	
 	public static final String PLUGINS_ONLINE="https://raw.githubusercontent.com/DisableSnow/SinkProGuard/master/plugin.json";
+	                                          
     public static boolean isProguarding=false;
 	
 }
